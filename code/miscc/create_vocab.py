00016795f7e180ec786202f6745c299d3d04e51c
Joseph KJ <josephkj20@gmail.com>
2018-04-19 10:32:55 +0530

    Evaluation of: passing multiple captions to each stage

---
+++ code/miscc/create_vocab.py
@@ -0,0 +1,85 @@
+import cPickle as pickle
+import os
+import nltk
+from collections import Counter
+
+class Vocabulary(object):
+    def __init__(self):
+        self.word2idx = {}
+        self.idx2word = {}
+        self.idx = 0
+
+    def add_word(self, word):
+        if not (word in self.word2idx):
+            self.word2idx[word] = self.idx
+            self.idx2word[self.idx] = word
+            self.idx += 1
+
+    def __call__(self, word):
+        if not word in self.word2idx:
+            return self.word2idx['<unk>']
+        return self.word2idx[word]
+
+    def __len__(self):
+        return self.idx
+
+
+def create_CUB_vocab(data_dir, caption_filenames, vocab_path, threshold=1):
+    """Creates a vocabulary for the CUB dataset.
+
+    """
+
+    # def load_captions(caption_name):  # self,
+    #     cap_path = caption_name
+    #     with open(cap_path, "r") as f:
+    #         captions = f.read().decode('utf8').split('\n')
+    #     captions = [cap.replace("\ufffd\ufffd", " ")
+    #                 for cap in captions if len(cap) > 0]
+    #     return captions
+
+    vocab = Vocabulary()
+    vocab.add_word('<pad>')
+    vocab.add_word('<start>')
+    vocab.add_word('<end>')
+    vocab.add_word('<unk>')
+
+    # Adding words from each caption
+    counter = Counter()
+    for i, key in enumerate(caption_filenames):
+        caption_name = '%s/text/%s.txt' % (data_dir, key)
+
+        with open(caption_name, "r") as f:
+            captions = f.read().decode('utf8').split('\n')
+
+        for caption in captions:
+            if len(caption) > 0:
+                caption = caption.replace("\ufffd\ufffd", " ")
+                tokens = nltk.tokenize.word_tokenize(caption.lower())
+                counter.update(tokens)
+
+        print('[%d/%d] Creating vocabulary.' %(i, len(caption_filenames)))
+
+    words = [word for word, cnt in counter.items() if cnt >= threshold]
+
+    for word in words:
+        vocab.add_word(word)
+
+    with open(vocab_path, 'wb') as f:
+        pickle.dump(vocab, f)
+
+    print('Created vocabulary. Total items are: %d' %(len(vocab)))
+
+
+def load_filenames(data_dir):
+    filepath = os.path.join(data_dir, 'train', 'filenames.pickle')
+    with open(filepath, 'rb') as f:
+        filenames = pickle.load(f)
+    print('Load filenames from: %s (%d)' % (filepath, len(filenames)))
+    return filenames
+
+
+if __name__ == '__main__':
+    data_dir = '../data/birds'
+    filenames = load_filenames(data_dir)
+    vocab_path = os.path.join(data_dir + 'cub_vocab.pkl')
+    create_CUB_vocab(data_dir, filenames, vocab_path)
